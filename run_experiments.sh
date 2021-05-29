b37727d914d63aaf14c4dff97c41ef4994061789
Saamahn Mahjouri <mahjouri.saamahn@gmail.com>
2021-05-28 22:10:17 -0500

    Update run_experiments.sh

--- run_experiments.sh
+++ run_experiments.sh
@@ -5,6 +5,17 @@
 ###  For this reason we recommend not uncommenting more      ###
 ###  than one line at a time.                                ###
 
+mkdir data
+mkdir results
+
+cd data
+wget https://zenodo.org/record/1127774/files/train.cpkl.gz
+wget https://zenodo.org/record/1127774/files/test.cpkl.gz
+
+gzip -d train.cpkl.gz
+gzip -d test.cpkl.gz
+cd ..
+
 python experiment_runner.py no_conv.yml
 #python experiment_runner.py single_weight_matrix.yml
 #python experiment_runner.py node_average.yml
